4a466eb4f68f97b505c7a4879a80a77fe84061c5
Abhishek raj singh <64788581+Avshek-raj@users.noreply.github.com>
2026-03-30 09:59:34 +0000

    Fixed issue where reloading page other than home page was not working

--- vite.config.js
+++ vite.config.js
@@ -6,12 +6,16 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
+    middlewareMode: false,
     proxy: {
       '/api/video-proxy': {
         target: 'http://localhost:3000',
         changeOrigin: true,
         secure: false,
       }
-    }
+    },
+  },
+  preview: {
+    middlewareMode: false,
   }
 })
